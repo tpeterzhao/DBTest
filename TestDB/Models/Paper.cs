75c83c4a8bd4fac4308dd8392de1b6e254e57442
jaronvg <77411227+jaronvg@users.noreply.github.com>
2021-07-14 12:16:03 -0400

    Update Paper.cs

--- TestDB/Models/Paper.cs
+++ TestDB/Models/Paper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,66 +23,66 @@ namespace TestDB.Models
         [StringLength(int.MaxValue)]
         public string NotesToReviewers { get; set; }
         [Display(Name = "Analysis of Algorithms")]
-        public bool AoA { get; set; }
+        public bool AnalysisOfAlgorithms { get; set; }
         public bool Applications { get; set; }
         public bool Architecture { get; set; }
         [Display(Name = "Artificial Intelligence")]
-        public bool AI { get; set; }
+        public bool ArtificialIntelligence { get; set; }
         [Display(Name = "Computer Engineering")]
-        public bool CE { get; set; }
+        public bool ComputerEngineering { get; set; }
         public bool Curriculum { get; set; }
         [Display(Name = "Data Structure")]
-        public bool DS { get; set; }
-        public bool Database { get; set; }
+        public bool DataStructures { get; set; }
+        public bool Databases { get; set; }
         [Display(Name = "Distance Learning")]
-        public bool DL { get; set; }
+        public bool DistanceLearning { get; set; }
         [Display(Name = "Distributed Systems")]
-        public bool DSS { get; set; }
+        public bool DistributedSystems { get; set; }
         [Display(Name = "Ethical Societal Issues")]
-        public bool ESI { get; set; }
+        public bool EthicalSocietalIssues { get; set; }
         [Display(Name = "First Year Computing")]
-        public bool FYC { get; set; }
+        public bool FirstYearComputing { get; set; }
         [Display(Name = "Gender Issues")]
-        public bool GI { get; set; }
+        public bool GenderIssues { get; set; }
         [Display(Name = "Grant Writing")]
-        public bool GW { get; set; }
+        public bool GrantWriting { get; set; }
         [Display(Name = "Graphics Image Processing")]
-        public bool GIP { get; set; }
+        public bool GraphicsImageProcessing { get; set; }
         [Display(Name = "Human Computer Interaction")]
-        public bool HCI { get; set; }
+        public bool HumanComputerInteraction { get; set; }
         [Display(Name = "Laboratory Environments")]
-        public bool LE { get; set; }
+        public bool LaboratoryEnvironments { get; set; }
         public bool Literacy { get; set; }
         [Display(Name = "Mathematics In Computing")]
-        public bool MIC { get; set; }
+        public bool MathematicsInComputing { get; set; }
         public bool Multimedia { get; set; }
         [Display(Name = "Networking Data Communication")]
-        public bool NDC { get; set; }
+        public bool NetworkingDataCommunications { get; set; }
         [Display(Name = "Non Major Courses")]
-        public bool NMC { get; set; }
+        public bool NonMajorCourses { get; set; }
         [Display(Name = "Object Oriented Issues")]
-        public bool OOI { get; set; }
+        public bool ObjectOrientedIssues { get; set; }
         [Display(Name = "Operating Systems")]
-        public bool OS { get; set; }
+        public bool OperatingSystems { get; set; }
         [Display(Name = "Parallel Processing")]
-        public bool PP { get; set; }
+        public bool ParallelsProcessing { get; set; }
         public bool Pedagogy { get; set; }
         [Display(Name = "Programming Languages")]
-        public bool PL { get; set; }
+        public bool ProgrammingLanguages { get; set; }
         public bool Research { get; set; }
         public bool Security { get; set; }
         [Display(Name = "Software Engineering")]
-        public bool SE { get; set; }
+        public bool SoftwareEngineering { get; set; }
         [Display(Name = "System Analysis And Design")]
-        public bool SAAD { get; set; }
+        public bool SystemsAnalysisAndDesign { get; set; }
         [Display(Name = "Using Technology In The Classroom")]
-        public bool UTITC { get; set; }
+        public bool UsingTechnologyInTheClassroom { get; set; }
         [Display(Name = "Web And Internet Programming")]
-        public bool WAIP { get; set; }
+        public bool WebAndInternetProgramming { get; set; }
         public bool Other { get; set; }
         [Display(Name = "Other Description")]
         [StringLength(50)]
-        public string OD { get; set; }
+        public string OtherDescription { get; set; }
 
 
 
