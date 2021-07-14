a393d620704d2637b631b5d6692fb4d113cc5449
jaronvg <77411227+jaronvg@users.noreply.github.com>
2021-07-14 15:38:03 -0400

    Update Review.cs

--- TestDB/Models/Review.cs
+++ TestDB/Models/Review.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,60 +14,60 @@ namespace TestDB.Models
         public int ReviewerID { get; set; }
         [Display(Name = "Appropriateness of Topic")]
         [Column(TypeName = "decimal(3,2)")]
-        public float AOT { get; set; }
+        public float AppropriatenessOfTopic { get; set; }
         [Display(Name = "Timeliness of Topic")]
         [Column(TypeName = "decimal(3,2)")]
-        public float ToT { get; set; }
+        public float TimelinessOfTopic { get; set; }
         [Display(Name = "Supportive Evidence")]
         [Column(TypeName = "decimal(3,2)")]
-        public float SE { get; set; }
+        public float SupportiveEvidence { get; set; }
         [Display(Name = "Technical Quality")]
         [Column(TypeName = "decimal(3,2)")]
-        public float TQ { get; set; }
+        public float TechnicalQuality { get; set; }
         [Display(Name = "Scope of Coverage")]
         [Column(TypeName = "decimal(3,2)")]
-        public float SoC { get; set; }
+        public float ScopeOfCoverage { get; set; }
         [Display(Name = "Citation of Previous Work")]
         [Column(TypeName = "decimal(3,2)")]
-        public float CoPW { get; set; }
+        public float CitationOfPreviousWork { get; set; }
 
         [Column(TypeName = "decimal(3,2)")]
         public float Originality { get; set; }
         [Display(Name = "Content Comments")]
         [StringLength(int.MaxValue)]
-        public string CC { get; set; }
+        public string ContentComments { get; set; }
         [Display(Name = "Organization of Paper")]
         [Column(TypeName = "decimal(3,2)")]
-        public float OoP { get; set; }
+        public float OrganizationOfPaper { get; set; }
         [Display(Name = "Clarity of Main Message")]
         [Column(TypeName = "decimal(3,2)")]
-        public float CoMM { get; set; }
+        public float ClarityOfMainMessage { get; set; }
         [Column(TypeName = "decimal(3,2)")]
         public float Mechanics { get; set; }
         [Display(Name = "Written Document Comments")]
         [StringLength(int.MaxValue)]
-        public string WDC { get; set; }
+        public string WrittenDocumentComments { get; set; }
         [Display(Name = "Suitability for Presentation")]
         [Column(TypeName = "decimal(3,2)")]
-        public float SfP { get; set; }
+        public float SuitabilityForPresentation { get; set; }
         [Display(Name = "Potential Interest in Topic")]
         [Column(TypeName = "decimal(3,2)")]
-        public float PIiT { get; set; }
+        public float PotentialInterestInTopic { get; set; }
         [Display(Name = "Potential for Oral Presentation Comments")]
         [StringLength(int.MaxValue)]
-        public string PfOPC { get; set; }
+        public string PotentialForOralPresentationComments { get; set; }
         [Display(Name = "Overall Rating")]
         [Column(TypeName = "decimal(3,2)")]
-        public float OR { get; set; }
+        public float OverallRating { get; set; }
         [Display(Name = "Overall Rating Comments")]
         [StringLength(int.MaxValue)]
-        public string ORC { get; set; }
+        public string OverallRatingComments { get; set; }
         [Display(Name = "Comfort Level Topic")]
         [Column(TypeName = "decimal(3,2)")]
-        public float CLT { get; set; }
+        public float ComfortLevelTopic { get; set; }
         [Display(Name = "Comfort Level Acceptability")]
         [Column(TypeName = "decimal(3,2)")]
-        public float CLA { get; set; }
+        public float ComfortLevelAcceptability { get; set; }
         public bool Complete { get; set; }
 
     }
